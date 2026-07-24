71edf9a3e2ee618d6af9b02315dac056e178ca26
mo <mo@gmail.com>
2026-07-24 15:58:14 -0400

    added teh top level system vhdl file

---
+++ hardware/system/system.vhdl
@@ -0,0 +1,91 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity system is
+
+  port (
+
+    -- FPGA clock
+    clk   : in std_logic;
+    reset : in std_logic;
+    -- Start reading sensor
+    start : in std_logic;
+    -- Debug output
+    device_id : out std_logic_vector(7 downto 0);
+    done      : out std_logic;
+    -- SPI pins to ADXL345
+    spi_cs   : out std_logic;
+    spi_clk  : out std_logic;
+    spi_mosi : out std_logic;
+    spi_miso : in std_logic
+
+  );
+
+end system;
+
+architecture rtl of system is
+  ------------------------------------------------
+  -- Signals between accelerometer controller
+  -- and SPI reader
+  ------------------------------------------------
+
+  signal spi_start_signal : std_logic;
+
+  signal spi_ready_signal : std_logic;
+  signal spi_tx_data_signal :
+  std_logic_vector(7 downto 0);
+  signal spi_rx_data_signal :
+  std_logic_vector(7 downto 0);
+
+begin
+
+  ------------------------------------------------
+  -- Accelerometer Controller
+  ------------------------------------------------
+
+  accel_controller : entity work.accelerometer_controller
+
+    port map
+    (
+
+      clk => clk,
+
+      reset => reset,
+
+      start       => start,
+      spi_start   => spi_start_signal,
+      spi_tx_data => spi_tx_data_signal,
+      spi_ready   => spi_ready_signal,
+      spi_rx_data => spi_rx_data_signal,
+      device_id   => device_id,
+      done        => done
+
+    );
+
+  ------------------------------------------------
+  -- SPI Reader
+  ------------------------------------------------
+
+  spi : entity work.spi_reader
+
+    port map
+    (
+
+      clk => clk,
+
+      reset => reset,
+      start => spi_start_signal,
+      busy  => open,
+
+      ready    => spi_ready_signal,
+      data_out => spi_rx_data_signal,
+      spi_cs   => spi_cs,
+
+      spi_clk => spi_clk,
+
+      spi_mosi => spi_mosi,
+
+      spi_miso => spi_miso
+
+    );
+
+end rtl;
